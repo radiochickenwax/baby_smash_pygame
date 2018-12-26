0d59834ea2690598c7ce2c7f64fe56947cd948a9
Greg2010iMac <gregorio.jp.mitchell@gmail.com>
2018-12-26 14:25:15 -0700

    starting example from pyglet docs

--- 00-taste.py
+++ 00-taste.py
@@ -1,25 +1,27 @@
 import sys, pygame
 pygame.init()
 
-size = width, height = 320, 240
-speed = [2, 2]
-black = 0, 0, 0
+print(pygame.display.Info());
+# size = width, height = 320, 240
 
-screen = pygame.display.set_mode(size)
+# speed = [2, 2]
+# black = 0, 0, 0
 
-ball = pygame.image.load("intro_ball.gif")
-ballrect = ball.get_rect()
+# screen = pygame.display.set_mode(size)
 
-while 1:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT: sys.exit()
+# ball = pygame.image.load("intro_ball.gif")
+# ballrect = ball.get_rect()
 
-    ballrect = ballrect.move(speed)
-    if ballrect.left < 0 or ballrect.right > width:
-        speed[0] = -speed[0]
-    if ballrect.top < 0 or ballrect.bottom > height:
-        speed[1] = -speed[1]
+# while 1:
+#     for event in pygame.event.get():
+#         if event.type == pygame.QUIT: sys.exit()
 
-    screen.fill(black)
-    screen.blit(ball, ballrect)
-    pygame.display.flip()
+#     ballrect = ballrect.move(speed)
+#     if ballrect.left < 0 or ballrect.right > width:
+#         speed[0] = -speed[0]
+#     if ballrect.top < 0 or ballrect.bottom > height:
+#         speed[1] = -speed[1]
+
+#     screen.fill(black)
+#     screen.blit(ball, ballrect)
+#     pygame.display.flip()
