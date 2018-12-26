ecf2c499d1e006d6acfe9e5550bcd6cab7b3c6b9
Greg2010iMac <gregorio.jp.mitchell@gmail.com>
2018-12-26 14:32:53 -0700

    starting text example from pyglet docs

--- 01-pyglet_hello_world.py
+++ 01-pyglet_hello_world.py
@@ -4,6 +4,12 @@ from pyglet.window import mouse
 
 window = pyglet.window.Window()
 
+label = pyglet.text.Label('Hello, world',
+                          font_name='Times New Roman',
+                          font_size=36,
+                          x=window.width//2, y=window.height//2,
+                          anchor_x='center', anchor_y='center')
+
 @window.event
 def on_key_press(symbol, modifiers):
     if symbol == key.A:
@@ -22,6 +28,7 @@ def on_mouse_press(x, y, button, modifiers):
 @window.event
 def on_draw():
     window.clear()
+    label.draw()
 
 pyglet.app.run()
 
